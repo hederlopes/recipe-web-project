167da5a2310a2f635c052b2dacfc0141bc4f712e
Heder Lopes <sistemas.heder@gmail.com>
2025-06-29 20:02:08 -0300

    remove contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Heder Lopes'
-    }
-)
-
-
-def contato(request):
-    return HttpResponse("<h1 style='color: yellow'><i>Contato</i></h1>")
-
-
-def sobre(request):
-    return HttpResponse("<h1 style='color: green'><i><a>Sobre</a></i></h1>")
+    })
